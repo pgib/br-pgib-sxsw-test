b9db48078de47f5c273c0396474f50a0b1cc68f7
Patrick Gibson <patrick@patrickg.com>
2015-03-14 12:12:21 -0500

    Implement triangle type checks.

--- js/TriangleClassifier.js
+++ js/TriangleClassifier.js
@@ -1,6 +1,21 @@
 var TriangleClassifier = function() {
 
-  this.classify = function() {
-    return "scalene";
+  this.classify = function(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if (a == b && b == c)
+    {
+      return "equilateral";
+    }
+    else if ((a == b && b != c) || (a != b && b == c))
+    {
+      return "isosceles";
+    }
+    else
+    {
+      return "scalene";
+    }
   };
 };
--- specs/TriangleClassifierSpec.js
+++ specs/TriangleClassifierSpec.js
@@ -21,5 +21,20 @@ describe('TriangleClassifier', function() {
     var result = new TriangleClassifier().classify(4,3,2);
     expect(result).toEqual('scalene');
   });
+
+  it('classifies as isosceles 4,4,2', function() {
+    var result = new TriangleClassifier().classify(4,4,2);
+    expect(result).toEqual('isosceles');
+  });
+
+  it('classifies as isosceles 2,4,4', function() {
+    var result = new TriangleClassifier().classify(2,4,4);
+    expect(result).toEqual('isosceles');
+  });
+
+  it('classifies as equilateral 4,4,4', function() {
+    var result = new TriangleClassifier().classify(4,4,4);
+    expect(result).toEqual('equilateral');
+  });
 });
 
